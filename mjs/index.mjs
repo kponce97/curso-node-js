f999ccd9b6d8524dae5e59aa83fd83f82ed22e6c
Kevin Elias <kevin_elias_ponce@hotmail.com>
2024-04-24 19:32:54 -0300

    include mjs cjs and apunte

---
+++ mjs/index.mjs
@@ -0,0 +1,8 @@
+// .js -> por defecto utilza CommonJS
+// .mjs -> para utilizar ES Modules 
+// .cjs -> para utilizar  CommonJS
+
+import { sum, sub } from "./sum.mjs";
+
+console.log(sum(1, 2));
+console.log(sub(4, 2));
